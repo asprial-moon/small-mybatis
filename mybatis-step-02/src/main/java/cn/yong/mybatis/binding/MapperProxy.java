ec99b03f4140a51fe615a4097902d9d62d6820ff
Allen <17317557314@163.com>
2022-08-28 11:55:06 +0800

    1

---
+++ mybatis-step-02/src/main/java/cn/yong/mybatis/binding/MapperProxy.java
@@ -0,0 +1,37 @@
+package cn.yong.mybatis.binding;
+
+import java.io.Serializable;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.util.Map;
+
+/**
+ * 映射器代理类
+ * 负责实现InvocationHandler接口的invoke方法，最终所有的实际调用都会调用到这个方法的包装的逻辑
+ * @author Allen
+ * @date 2022/8/28
+ */
+public class MapperProxy<T> implements InvocationHandler, Serializable {
+
+    private static final long serialVersionUID = -6424540398559729838L;
+
+    private Map<String, String> sqlSession;
+
+    private final Class<T> mapperInterface;
+
+    public MapperProxy(Map<String, String> sqlSession, Class<T> mapperInterface) {
+        this.sqlSession = sqlSession;
+        this.mapperInterface = mapperInterface;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+
+        if (Object.class.equals(method.getDeclaringClass())) {
+            return method.invoke(this, args);
+        } else {
+            return "你被代理了！" + sqlSession.get(mapperInterface.getName() + "." + method.getName());
+        }
+    }
+
+}
